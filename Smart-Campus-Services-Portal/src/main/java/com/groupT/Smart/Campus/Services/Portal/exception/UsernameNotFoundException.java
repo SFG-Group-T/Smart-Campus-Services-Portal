f07c5b82bf893a56b957222c86be76d278fbe97e
Moribo12 <91187139+Moribo12@users.noreply.github.com>
2025-04-21 21:02:19 +0200

    Delete Smart-Campus-Services-Portal directory

--- Smart-Campus-Services-Portal/src/main/java/com/groupT/Smart/Campus/Services/Portal/exception/UsernameNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.groupT.Smart.Campus.Services.Portal.exception;
-
-public class UsernameNotFoundException extends RuntimeException{
-
-    public UsernameNotFoundException(String message) {
-        super(message);
-    }
-}
--- Smart-Campus-Services-Portal/src/main/java/com/groupT/Smart/Campus/Services/Portal/util/Role.java
+++
@@ -1,5 +0,0 @@
-package com.groupT.Smart.Campus.Services.Portal.util;
-
-public enum Role {
-    STUDENT,ADMIN,LECTURER
-}
